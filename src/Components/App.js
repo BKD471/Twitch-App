273798b9e1d108f9d70993d45cbb13d40334567e
BKD471 <bhaskarkumardas9@gmail.com>
2022-03-14 01:05:05 +0530

    rout

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const pageOne = () => {
+  return (
+    <div>
+      Page 1<Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      Page 2<Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={pageOne} />
+        <Route path="/pagetwo" component={pageTwo} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
